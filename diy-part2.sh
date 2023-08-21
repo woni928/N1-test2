634d6a10272b952ff7ed53c444f6e2c0bd3130cd
ham0223 <126130711+ham0223@users.noreply.github.com>
2023-08-21 11:53:39 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+
+# 增加 alist （在 ./scripts/feeds install -a 操作之后更换 golang 版本）
+#rm -rf feeds/packages/lang/golang
+#svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+
+# tmp fix-20230819之前编译失败回滚配置
+wget -O ./package/kernel/linux/modules/netsupport.mk https://raw.githubusercontent.com/coolsnowwolf/lede/3ef1f5ade3b8f6527bbc4eb9494138de66e07d13/package/kernel/linux/modules/netsupport.mk
